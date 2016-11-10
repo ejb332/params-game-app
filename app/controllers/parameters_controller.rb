6f3de3851508f700e952a688e778a50b3964431b
Eric Biel <eric.j.biel@gmail.com>
2016-11-09 19:51:11 -0600

    Complete app-name and guess-a-number game

--- app/controllers/parameters_controller.rb
+++ app/controllers/parameters_controller.rb
@@ -1,6 +1,22 @@
 class ParametersController < ApplicationController
   def parameters_game_method
-    @app_name = params['app_name']
+    @app_name = params['app_name'].upcase
+      if @app_name[0] == 'A'
+        @app_name = "Hey, your name starts with the first letter of the alphabet!"
+      end
     render 'parameters-game.html.erb'
+  end
+
+  def guess_number_method
+    @guess = params['guess']
+    @answer = params['answer']
+    if @guess.to_i > 42
+      @answer = "Guess a lower number"
+    elsif @guess.to_i < 42
+      @answer = "Guess a higher number."
+    else
+      @answer = "That's it! 42"
+    end
+    render 'guess-nums.html.erb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/parameters-game' => 'parameters#parameters_game_method'
+  get '/guess-nums' => 'parameters#guess_number_method'
 end
